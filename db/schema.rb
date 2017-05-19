7f1cc0faacc3645707d5557f1dd02a7292931f91
kai <328458211@qq.com>
2017-05-19 15:11:28 +0800

    order by gem acts_as_list

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170518092655) do
+ActiveRecord::Schema.define(version: 20170519032431) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20170518092655) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "image"
+    t.integer  "position"
   end
 
   create_table "users", force: :cascade do |t|
